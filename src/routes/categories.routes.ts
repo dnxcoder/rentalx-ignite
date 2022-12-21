773418361f252e2b44a9e0525fcd50950bf6b942
Denis Coder <dnxcoder@gmail.com>
2022-12-20 23:21:33 -0600

    Inserting typing to category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,23 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
+  const category = new Category();
+
+  //inserting values to the properties of object category
+  Object.assign(category, {
     name,
     description,
-  };
+    created_at: new Date(),
+  });
 
-  categories.push(category);
-
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
