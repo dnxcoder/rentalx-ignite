1b790be1617ffd7e66d6f54206040a84c89ef013
Denis Coder <dnxcoder@gmail.com>
2022-12-20 23:47:18 -0600

    creating a repository of category

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,30 @@
+import { Category } from "../model/Category";
+
+//DTO => Data transfer object
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    //inserting values to the properties of object category
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
