5d4d9be5c422865fe389d96952c958da8f679c01
Denis Coder <dnxcoder@gmail.com>
2022-12-20 21:08:16 -0600

    debuging the aplication

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,20 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   console.log("ALOU");
 
   response.send("tudo de boa na lagoa");
 });
 
-app.listen(3333);
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
+app.listen(3333, () => {
+  console.log("Server is running!");
+});
