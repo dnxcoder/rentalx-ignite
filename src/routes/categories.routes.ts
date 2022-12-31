4140ec49b6a5b6a81d489b3b5b6377ddf7e837cd
Denis Coder <dnxcoder@gmail.com>
2022-12-31 14:16:08 -0600

    refactoring the listing of categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 //import { PostgressCategoriesRepository } from "../modules/cars/repositories/PostgressCategoriesRepository";
 //import { CreateCategoryService } from "../modules/cars/useCases/createCategory/CreateCategoryUseCase";
 //import { ListCategoryService } from "../modules/cars/services/ListCategoryService";
@@ -13,6 +14,8 @@ categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get("/", (request, response) => {});
+categoriesRoutes.get("/", (request, response) => {
+  return listCategoriesController.handle(request, response);
+});
 
 export { categoriesRoutes };
