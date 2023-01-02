12f71b9136b1cbf606ecce7ec66166d22d72e3d7
Denis Coder <dnxcoder@gmail.com>
2023-01-02 00:22:01 -0600

    creating the concept of stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,18 @@
+import fs from "fs";
+import { parse as csvParse } from "csv-parse";
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File) {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    // pipe takes the chunk of the file that was read and put inside of csvParse
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
