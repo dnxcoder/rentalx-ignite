6c14e3dc46051f67e6b084c37dc8d99a2c727ac7
Denis Coder <dnxcoder@gmail.com>
2023-02-15 18:56:25 -0600

    fixed docker-compose file for server and database stay in the same ip

--- src/server.ts
+++ src/server.ts
@@ -13,5 +13,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(router);
 
 app.listen(3333, () => {
-  console.log("Server is running!");
+  console.log("Server is running!!!!!");
 });
