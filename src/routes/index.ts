340d27e7aa38f84ee0542daade4ea3fb14dee2d0
Denis Coder <dnxcoder@gmail.com>
2023-03-18 17:42:53 -0500

    creating controller of user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { categoriesRoutes } from "./categories.routes";
 import { speficationsRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", speficationsRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
